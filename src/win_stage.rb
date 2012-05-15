8f0f61eb290480ad239821cbcf1fe1e45f691fb8
Shawn Anderson <shawn42@gmail.com>
2012-05-15 17:36:57 -0400

    cleaned up win_stage

--- src/win_stage.rb
+++ src/win_stage.rb
@@ -2,15 +2,10 @@ class WinStage < Stage
 
   def setup
     super
-    create_actor :logo, :x => 480, :y => 360
-    create_actor :label, :text => "YOU WIN!", :x => 150, :y => 100, :size => 90
-    create_actor :label, :text => "SCORE:", :x => 40, :y => 200, :size => 90
-    create_actor :label, :text => backstage[:score], :x => 300, :y => 200, :size => 90
-  end
-
-  def draw(target)
-    target.fill [0]*3
-    super
+    create_actor :logo, x: 480, y: 360
+    create_actor :label, text: "YOU WIN!", x: 150, y: 100, size: 90
+    create_actor :label, text: "SCORE:", x: 40, y: 200, size: 90
+    create_actor :label, text: backstage[:score], x: 300, y: 200, size: 90
   end
 
 end
